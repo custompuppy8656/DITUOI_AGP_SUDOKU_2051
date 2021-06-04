79a237aaa80166e43c6caba93b8c2247d1edb675
NikolaosSarris <65713677+custompuppy8656@users.noreply.github.com>
2021-06-05 00:33:19 +0300

    new file

--- assignment.py
+++ assignment.py
@@ -1,9 +1,9 @@
-# Πρότυπο εργασίας
+
 from sudokugame import *
 
 def main():
     inputdata=Input('sudokusamples.txt')
-    #Όταν έχετε έτοιμο τον κώδικα για το Input function διαγράφεται την παρακάτω γραμμή
+
 
     for sudoku in inputdata:
        print(f'Sequence used:{sudoku}\n')
